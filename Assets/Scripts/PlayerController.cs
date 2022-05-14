2c73c8a9cd92fb02952cbbcfc80b8427fecf9f11
Iván L. Murillo <diskman199@gmail.com>
2022-05-14 10:34:43 -0500

    Working on Android

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,12 +9,11 @@ public class PlayerController : MonoBehaviour
     public float speed = 10.0f;
     public float xRange = 20.0f;
     public float zRange = 10.0f;
-    
+
     public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -29,7 +28,15 @@ public class PlayerController : MonoBehaviour
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
         horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        if(Input.touchCount > 0)
+        {
+            float phoneHorizontal = Input.GetTouch(0).deltaPosition.x;
+            transform.Translate(Vector3.right * phoneHorizontal * Time.deltaTime * speed);
+        }
+        else
+        {
+            transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        }
 
         if(transform.position.z < -zRange)
         {
@@ -38,11 +45,21 @@ public class PlayerController : MonoBehaviour
         if(transform.position.z > zRange)
         {
             transform.position = new Vector3(transform.position.x, transform.position.y, zRange);
-        }        
+        }
+
         verticalInput = Input.GetAxis("Vertical");
-        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);        
+        if(Input.touchCount > 0)
+        {
+            float phoneVertical = Input.GetTouch(0).deltaPosition.y;
+            Debug.Log("Phone vertical " + phoneVertical);
+            transform.Translate(Vector3.forward * phoneVertical * Time.deltaTime * speed);        
+        }
+        else
+        {
+            transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);        
+        }        
         
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Space) || Input.touchCount > 0)
         {
             Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
